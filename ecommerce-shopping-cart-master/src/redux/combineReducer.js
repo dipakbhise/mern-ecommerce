2bf1f912dd6975c34b54a386a58ec9aa982ee774
Dipak Bhise <dipakbhise420@gmail.com>
2021-12-19 22:05:18 +0530

    Delete ecommerce-shopping-cart-master directory

--- ecommerce-shopping-cart-master/src/redux/combineReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import shopReducer from "./Shopping/shopping-reducer"
-
-
-const combineReducer = combineReducers({
-    shop : shopReducer
-})
-
-export default combineReducer;
--- ecommerce-shopping-cart-master/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import combineReducer from "./combineReducer"
-import { composeWithDevTools } from "redux-devtools-extension";
-
-const store = createStore(combineReducer, composeWithDevTools())
-
-
-export default store;
